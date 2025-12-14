139ca0ce98b2e58662552cf17019337f0da85cef
mayureshraval <mayureshraval2012@gmail.com>
2025-12-14 19:23:58 +0530

    Added xpath

--- cypress/e2e/Automation-test-store/contact-us.js
+++ cypress/e2e/Automation-test-store/contact-us.js
@@ -2,7 +2,13 @@
  
 
 describe('Test Contact us via Automation Test Store', () => {
-    it('Successfull', () => {
+    it('Successful', () => {
         cy.visit('https://automationteststore.com/')
+        // cy.get('ul.info_links_footer').contains('a','Contact Us').click()
+        cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click()
+        cy.get('#ContactUsFrm_first_name').type('Mayuresh Rawal')
+        cy.get('#ContactUsFrm_email').type('mayuresh@gmail.com')
+        cy.get('#ContactUsFrm_enquiry').type('How to order Products?')
+        cy.get('#ContactUsFrm  div.form-group button[type="submit"]').click()
     });
 }); 
