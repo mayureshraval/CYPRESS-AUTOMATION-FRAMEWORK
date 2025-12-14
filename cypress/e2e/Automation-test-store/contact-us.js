390aa9d4847183f83c9528643b403a1172b0f888
mayureshraval <mayureshraval2012@gmail.com>
2025-12-14 12:33:36 +0530

    Automation test store init

---
+++ cypress/e2e/Automation-test-store/contact-us.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress"/>
+ 
+
+describe('Test Contact us via Automation Test Store', () => {
+    it('Successfull', () => {
+        cy.visit('https://automationteststore.com/')
+    });
+}); 
